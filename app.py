80aba84150d3f8f7121855f5dd91bbeee9d86149
DJ654312 <89970856+DJ654312@users.noreply.github.com>
2025-01-19 15:50:38 +0530

    app.py
    updated the host address and port number

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def reviews():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host='0.0.0.0', port=port)
